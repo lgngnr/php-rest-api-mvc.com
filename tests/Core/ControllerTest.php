f200f330212cc35c81bf81119c0a1da45ebc94b5
Luigi <luiginigrelli@me.com>
2019-07-20 15:06:21 +0200

    Controller Unit Test base implemented

---
+++ tests/Core/ControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+    use Vendor\Core\Controller;
+
+    /**
+     * ControllerTest class
+     * Unit Test: model(), view()
+     */
+    class ControllerTest extends TestCase
+    {
+
+        /**
+         * testModel function
+         * It should load the model requested in param
+         * @testd
+         */
+        public function testModel()
+        {
+            $this->markTestIncomplete(
+                'This test has not been implemented yet.'
+              );
+        }
+
+        /**
+         * testView function
+         * It should load the view required in param
+         * @test
+         */
+        public function testView()
+        {
+            $this->markTestIncomplete(
+                'This test has not been implemented yet.'
+              );
+        }
+    }
+?>
