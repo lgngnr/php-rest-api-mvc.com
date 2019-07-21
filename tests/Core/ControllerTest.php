e118b28dde83bf0d3410dcb5b2ad438eb4dcc3c2
Luigi <luiginigrelli@me.com>
2019-07-21 19:20:41 +0200

    Core\Controller Unit Tests for model() 7 view() implemented

--- tests/Core/ControllerTest.php
+++ tests/Core/ControllerTest.php
@@ -1,24 +1,47 @@
 <?php
     use PHPUnit\Framework\TestCase;
     use Vendor\Core\Controller;
+    use Vendor\Models\Product;
 
+    // Load config
+    require_once dirname(dirname(__DIR__)) . "/App/Vendor/Config/config.php";
+    
     /**
      * ControllerTest class
      * Unit Test: model(), view()
      */
     class ControllerTest extends TestCase
     {
+        private const INDEX_FILE = APP_ROOT . "/Vendor/Views/home/index.php";
+        private $controller;
+
+        /**
+         * setUp function
+         * Create an istance of Controller
+         * @return void
+         */
+        public function setUp() : void
+        {
+            $this->controller = new Controller;
+        }
 
         /**
          * testModel function
-         * It should load the model requested in param
-         * @testd
+         * It should load the model requested in param if exist
+         * It shouldn't load the model if not exist
+         * 
          */
         public function testModel()
         {
-            $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-              );
+            $model = $this->controller->model('Product');
+            // Should get a model istance
+            $this->assertNotNull($model);
+            // Should be a Models\Product istance
+            $this->assertInstanceOf(Product::class, $model);
+
+            $model = $this->controller->model('XXX');
+            // Should get a null
+            $this->assertNull($model);
         }
 
         /**
@@ -28,9 +51,18 @@
          */
         public function testView()
         {
-            $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-              );
+            // It should load home/index view
+            $view = $this->controller->view('home/index');
+            
+            // Loading Views/home/index.php
+            $indexFile = $this->loadView(self::INDEX_FILE);
+
+            // $view shlould contain index.php content
+            $this->assertEquals($indexFile, $view);
+        }
+
+        private function loadView($file){
+            require_once $file;
         }
     }
 ?>
