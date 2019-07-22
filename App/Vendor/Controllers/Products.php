994d2980fa12dd8a55962b2c392e1b3233fa5cad
Luigi <luiginigrelli@me.com>
2019-07-22 09:47:23 +0200

    Products CONTROLLER and all() method implemented

---
+++ App/Vendor/Controllers/Products.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Vendor\Controllers;
+use Vendor\Core as Core;
+
+/**
+ * Product Controller class
+ * Handle CRUD ops for products
+ */
+class Products extends Core\Controller
+{
+    private $productModel;
+
+    /**
+     * __construct function
+     * Istantiate Product Model
+     */
+    public function __construct()
+    {
+        $this->productModel = $this->model('Product');
+    }
+
+    /**
+     * all function
+     * It gets all products and load products/all view passing $data[]
+     * @return load requested view
+     */
+    public function all($items = null, $page = null)
+    {
+        // Get all products, format array[] of obj
+        $data = $this->productModel->readAll();
+        $this->view("products/all", $data);
+    }
+}
