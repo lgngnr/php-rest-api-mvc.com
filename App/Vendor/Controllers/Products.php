6791dd3ac3efb38d0cdd57eb18be7eb1a58800cd
Luigi <luiginigrelli@me.com>
2019-07-22 14:36:10 +0200

    Product CONTROLLER read, read VIEW implemented

--- App/Vendor/Controllers/Products.php
+++ App/Vendor/Controllers/Products.php
@@ -23,6 +23,8 @@ class Products extends Core\Controller
     /**
      * all function
      * It gets all products and load products/all view passing $data[]
+     * @param integer $page - page number
+     * @param integer $items - items/page
      * @return load requested view
      */
     public function all($page = null, $items = null)
@@ -33,5 +35,25 @@ class Products extends Core\Controller
         // Get all products, format array[] of obj
         $data = $this->productModel->readAll($page, $items);
         $this->view("products/all", $data);
+    }
+
+    /**
+     * read cRud function
+     * It gets all products and load products/all view passing $data[]
+     * @param integer $id - product id
+     * 
+     * @return load requested view
+     */
+    public function read($id)
+    {
+        // Sanitize $id
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        // Get product, format obj Popo\Product
+        $product = $this->productModel->read($id);
+        $data = array(
+            'data' => $product->__getObj()
+        );
+        $this->view("products/read", $data);
     }
 }
---
+++ App/Vendor/Views/products/read.php
@@ -0,0 +1,17 @@
+<?php
+    /** HEADERS */
+    header('Access-Control-Allow-Origin: *'); // public api
+    header('Content-Type: application/json');
+    
+    
+    if($data){
+        
+        // Encode $data to json & output 
+        echo json_encode($data);
+    }else{
+        // No Products
+        echo json_encode(
+            array('message' => 'No products found')
+        );
+    }
+?>
