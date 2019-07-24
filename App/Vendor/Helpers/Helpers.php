86c3c8ba9267e58388a247b4f174ec695089f942
Luigi <luiginigrelli@me.com>
2019-07-24 15:11:39 +0200

    Header functions helpers implemented

--- App/Vendor/Helpers/Helpers.php
+++ App/Vendor/Helpers/Helpers.php
@@ -20,5 +20,42 @@
         {
             header('location: /' . $path);
         }
+
+        /**
+         * unauthorized function
+         * send 401 Unauthorized back
+         * @return void
+         */
+        public static function unauthorized(){
+            http_response_code(401);
+        }
+
+        /**
+         * methodNotAllowed function
+         * 405 Method Not Allowed back
+         * @param array ...$methods method allowed GET,POST,...
+         * @return void
+         */
+        public static function methodNotAllowed(...$methods)
+        {
+            http_response_code(405);
+            $allowed = '';
+            foreach($methods as $method){
+                $allowed .= "$method,";
+            }
+            // Strip last ,
+            $allowed = rtrim($allowed, ',');
+            header('Access-Control-Allow-Method: ' . $allowed);
+        }
+
+        /**
+         * authorized function
+         * 405 Method Not Allowed back
+         * @param array ...$methods method allowed GET,POST,...
+         * @return void
+         */
+        public static function authorized($token){
+            header("Authorization: Bearer $token");
+        }
     }
 ?>
